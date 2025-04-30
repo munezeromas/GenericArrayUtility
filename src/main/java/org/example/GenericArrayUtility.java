5fdef453536497de9653718672cdf0ff4486a8a0
munezeromas <munezeromas@gmail.com>
2025-04-30 20:22:38 +0200

    commit

--- src/main/java/org/example/GenericArrayUtility.java
+++ src/main/java/org/example/GenericArrayUtility.java
@@ -1,4 +1,24 @@
 package org.example;
+import java.lang.reflect.Array;
 
 public class GenericArrayUtility {
+
+    public static <T> T[] mergeArrays(T[] a, T[] b) {
+        if (a == null && b == null) {
+            return (T[]) new Object[0];
+        } else if (a == null) {
+            return b;
+        } else if (b == null) {
+            return a;
+        }
+
+        int totalLength = a.length + b.length;
+        T[] result = (T[]) Array.newInstance(a.getClass().getComponentType(), totalLength);
+
+        System.arraycopy(a, 0, result, 0, a.length);
+        System.arraycopy(b, 0, result, a.length, b.length);
+
+        return result;
+    }
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,16 @@
 package org.example;
+import java.util.Arrays;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Integer[] nums1 = {1, 2, 3};
+        Integer[] nums2 = {4, 5, 6};
+        Integer[] mergedNums = GenericArrayUtility.mergeArrays(nums1, nums2);
+        System.out.println("Merged Integer Array: " + Arrays.toString(mergedNums));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String[] words1 = {"apple", "banana"};
+        String[] words2 = {"cherry", "date"};
+        String[] mergedWords = GenericArrayUtility.mergeArrays(words1, words2);
+        System.out.println("Merged String Array: " + Arrays.toString(mergedWords));
     }
 }
